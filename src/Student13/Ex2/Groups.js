837e094adfa9d1a66d0a787b7875e8f52edb501f
Rosie Gonzalez <rgonzalez@xpanxion.com>
2023-05-17 14:37:31 -0800

    Example 2

---
+++ src/Student13/Ex2/Groups.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Groups = () => {
+  return (
+    <div className='Groups'>
+       <h2>Groups</h2> 
+       <p>Likes: 12 cats</p>
+       <p>Likes: 2 Dogs</p>
+       <p>Likes: 0 Good Neighbors</p>
+    </div>
+  )
+}
